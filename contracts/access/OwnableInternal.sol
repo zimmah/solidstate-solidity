cff7b183e89abb9e59b512d3473b124bba424e37
Zimri Leijen <zimmah@gmail.com>
2021-06-05 18:02:40 +0200

    add owner getter

--- contracts/access/OwnableInternal.sol
+++ contracts/access/OwnableInternal.sol
@@ -7,6 +7,10 @@ import './OwnableStorage.sol';
 abstract contract OwnableInternal {
   using OwnableStorage for OwnableStorage.Layout;
 
+  function owner () virtual public view returns (address) {
+    return OwnableStorage.layout().owner;
+  }
+
   modifier onlyOwner {
     require(
       msg.sender == OwnableStorage.layout().owner,
